d4a7cac1bb91e9aa76299e44d7d3e19bb1004fb5
saugatnp <saugatnp11@gmail.com>
2021-07-09 09:46:34 +0545

    gallery database connected

--- resources/views/backends/examroutine.blade.php
+++ resources/views/backends/examroutine.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="mainback">
-    <h1>Gallery</h1>
+    <h1>Routine</h1>
     <div class="back">
         <table cellpadding="0" style="width:100%;border: 1px solid black;">
             <tr>
@@ -18,10 +18,23 @@
                             <i class="fa fa-edit"></i>
                             &nbsp;Edit
                         </a>
+                        {{-- code to delete routine --}}
+                        {!!Form::open(['action'=>['PostController@destroy' ,$data->id ], 'method'=>'POST','class'=>'pull-right '])!!}
+                        {{Form::hidden('_method' ,'DELETE')}}
+                        <button type="submit" name="Delete" value="Delete" class = "btn btn-primary">Delete</button> 
+                        {!!Form::close()!!}
                     </td>
                 </tr>
             @endforeach
         </table>
+        {{--  add new album --}}
+        {!! Form::open(array('action' => 'HomeController@addalbum', 'method' => 'POST')) !!}
+        <div class="form-group">
+            {{ Form::label('title', 'Add new routine') }}
+            {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Add new routine']) }}
+        </div>
+        {{ Form::submit('Add Routine', ['class' => 'btn btn-primary']) }}
+        {!! Form::close() !!}
     </div>
 </div>
 @endsection
--- resources/views/backends/gallery.blade.php
+++ resources/views/backends/gallery.blade.php
@@ -15,14 +15,28 @@
                     <tr>
                         <td></td>
                         <td>{{ $data->title }}</td>
-                        <td><a class="btn btn-success " href="/index/{{ $data->id }}/edit">
+                        <td><a class="btn btn-primary " href="/index/{{ $data->id }}/edit">
                                 <i class="fa fa-edit"></i>
                                 &nbsp;Edit
                             </a>
+                            {{-- code to delete album --}}
+                            {!!Form::open(['action'=>['PostController@destroy' ,$data->id ], 'method'=>'POST','class'=>'pull-right '])!!}
+                            {{Form::hidden('_method' ,'DELETE')}}
+                            <button type="submit" name="Delete" value="Delete" class = "btn btn-primary">Delete</button> 
+                            {{-- {{Form::submit('Delete' , ['class' => 'btn btn-primary'])}} --}}
+                            {!!Form::close()!!}
                         </td>
                     </tr>
                 @endforeach
             </table>
+            {{--  add new album --}}
+            {!! Form::open(array('action' => 'HomeController@addalbum', 'method' => 'POST')) !!}
+        <div class="form-group">
+            {{ Form::label('title', 'Add new album') }}
+            {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Add new album']) }}
+        </div>
+        {{ Form::submit('Add Album', ['class' => 'btn btn-primary']) }}
+        {!! Form::close() !!}
         </div>
     </div>
 @endsection
